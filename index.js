ac72163ddea3c344a94400784c60fcfb44c0211b
Juneid Teklu <te.juneid@gmail.com>
2024-03-12 18:42:39 -0500

    Create the operation functions

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(minuend,subtrahend){
+
+    return mineund - subtrahend;
+
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(divisor, dividend){
+    return dividend/divisor;
+}
+function square(base,exponent){
+    return base ** exponent;
+
+}
