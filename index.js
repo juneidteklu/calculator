87bd70cfae741126f7a05b9a507ba8160a28f96d
Juneid Teklu <te.juneid@gmail.com>
2024-03-12 19:35:29 -0500

    Start creating a way to store the value of number selection

--- index.js
+++ index.js
@@ -3,19 +3,28 @@ function add(num1, num2){
 }
 
 function subtract(minuend,subtrahend){
-
-    return mineund - subtrahend;
-
+    return minuend - subtrahend;
 }
 
 function multiply(num1, num2){
     return num1*num2;
 }
 
-function divide(divisor, dividend){
+function divide(dividend, divisor){
     return dividend/divisor;
 }
-function square(base,exponent){
-    return base ** exponent;
 
+function square(base,exponent){
+    return base**exponent;
 }
+
+const numberSelection = document.querySelector(".numbers");
+const operationSelection = document.querySelector(".operations");
+
+let firstNumber = "";
+numberSelection.addEventListener(("click"), (event)=> {
+    firstNumber = event.target.textContent;
+})
+console.log(firstNumber)
+
+
